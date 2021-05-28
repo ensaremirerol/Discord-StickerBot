3c289135662cf98d6a2421ab991d4d83a308c89a
Ensar Emir EROL <ensaremir.erol99@gmail.com>
2021-05-28 13:19:44 +0300

    Added webp format to images

--- index.js
+++ index.js
@@ -166,5 +166,5 @@ const isStickerInGuild = async (guildId, name, next) => {
 
 function isImgLink(url) {
 	if (typeof url !== 'string') return false;
-	return (url.match(/^http[^\?]*.(jpg|jpeg|gif|png|tiff|bmp)(\?(.*))?$/gmi) != null);
+	return (url.match(/^http[^\?]*.(jpg|jpeg|gif|png|tiff|bmp|webp)(\?(.*))?$/gmi) != null);
 }
