eeb48f84ca091800c7cc00a258e5652c4e3ee953
Ensar Emir EROL <ensaremir.erol99@gmail.com>
2021-06-04 14:07:15 +0300

    Fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({
     extended: true
 }));
-app.use(express.json());
+app.use(express.json({limit: "50mb"}));
 
 mongoose.connect(process.env.HOST_NAME, {
 	useNewUrlParser: true,
